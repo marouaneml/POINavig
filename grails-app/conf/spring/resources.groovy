9d178ba140f0f50c13a7d4e289f270e856f617fe
marouaneml <marouane-93@outlook.com>
2017-09-16 01:20:30 +0200

    Add Spring Security plugin ..

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import poinavig.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }

4b5f9edf98b077003ef141317c8368da58270207
marouaneml <marouane-93@outlook.com>
2017-09-19 16:20:48 +0200

    Correction poiGroupe

--- grails-app/domain/m2/mbds/projet/grails/poinavig/model/PoiGroupe.groovy
+++ grails-app/domain/m2/mbds/projet/grails/poinavig/model/PoiGroupe.groovy
@@ -1,10 +1,14 @@
 package m2.mbds.projet.grails.poinavig.model
 
+import m2.mbds.projet.grails.poinavig.User
+
 class PoiGroupe {
     /*
     many pois for user : un utilisateur possede plusieurs points d'interet
      */
     static hasMany = [pois: Poi]
+
+    static belongsTo = [user: User]
     static constraints = {
     }
 

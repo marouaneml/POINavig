dad8879fc4e825f4f7ab91541c93d1d316dde0be
marouaneml <marouane-93@outlook.com>
2017-09-19 14:04:34 +0200

    initiliser le git

--- grails-app/conf/spring/resources.groovy
+++
@@ -1,5 +0,0 @@
-import poinavig.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
-}

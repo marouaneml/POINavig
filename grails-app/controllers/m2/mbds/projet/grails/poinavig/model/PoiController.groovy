f9503f2e8f7d6bea82c4be130da04efb1d48101b
Lilianeo <cl_hames@esi.dz>
2017-09-26 20:57:44 +0200

    Ajout des liens entre le bootstrap et les controleurs et les vues

--- grails-app/controllers/m2/mbds/projet/grails/poinavig/model/PoiController.groovy
+++ grails-app/controllers/m2/mbds/projet/grails/poinavig/model/PoiController.groovy
@@ -5,8 +5,9 @@ import grails.plugin.springsecurity.annotation.Secured
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
+
 @Transactional(readOnly = true)
-@Secured("ROLE_ADMIN")
+@Secured(["ROLE_ADMIN", "ROLE_MEDERATEUR"])
 class PoiController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
--- grails-app/init/poinavig/BootStrap.groovy
+++ grails-app/init/poinavig/BootStrap.groovy
@@ -7,17 +7,32 @@ import m2.mbds.projet.grails.poinavig.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-        def admin = new User(username: 'admin', password: 'admin', enabled: true).save()
+        /*def admin = new User(username: 'admin', password: 'admin', enabled: true).save()
         def moderateur = new User(username: 'moderateur', password: 'moderateur', enabled: true).save()
         def user = new User(username: 'user', password: 'user', enabled: true).save()
-
         def adminRole = new Role(authority: 'ROLE_ADMIN').save()
         def moderateurRole = new Role(authority: 'ROLE_MODERATEUR').save()
         def userRole = new Role(authority: 'ROLE_USER').save()
-
         UserRole.create admin, adminRole, true
         UserRole.create moderateur, moderateurRole, true
-        UserRole.create user, userRole, true
+        UserRole.create user, userRole, true*/
+        def adminRole = Role.findOrSaveWhere('authority': 'ROLE_ADMIN')
+        def moderateurRole = Role.findOrSaveWhere('authority': 'ROLE_MODERATEUR')
+        def userRole = Role.findOrSaveWhere('authority': 'ROLE_USER')
+
+        def admin = User.findOrSaveWhere(username: 'admin', password: 'admin', enabled: true)
+        def moderateur = User.findOrSaveWhere(username: 'moderateur', password: 'moderateur', enabled: true)
+        def user = User.findOrSaveWhere(username: 'user', password: 'user', enabled: true)
+
+        if(!admin.authorities.contains(adminRole)){
+            UserRole.create(admin, adminRole, true)
+        }
+        if(!moderateur.authorities.contains(moderateurRole)){
+            UserRole.create(moderateur, moderateurRole, true)
+        }
+        if(!user.authorities.contains(userRole)){
+            UserRole.create(user, userRole, true)
+        }
     }
     def destroy = {
     }

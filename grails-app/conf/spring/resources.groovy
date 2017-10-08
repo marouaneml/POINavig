d49e5cfe665237b08f0c1ad78256c7e7b22584c7
marouaneml <marouane-93@outlook.com>
2017-10-08 22:56:49 +0200

    Correction des conflits

--- grails-app/conf/spring/resources.groovy
+++
@@ -1,6 +0,0 @@
-import m2.mbds.projet.grails.poinavig.UserPasswordEncoderListener
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
-}
